c8c816c93180912e22b8829c5001a93cbe603a8b
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2026-05-31 09:46:53 +0300

    modified a catalog component and about section

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -8,7 +8,7 @@ export default function Catalog(){
     const products = storeData.products || [];
     return (
         <section id="catalog" className="w-full h-screen flex flex-col items-center justify-start">
-          <h2 className="text-3xl font-bold font-playfairDisplay w-full px-10 text-dark-accent">The Collection</h2>
+          <h2 className="text-3xl font-bold font-playfairDisplay w-full px-5 text-dark-accent">The Collection</h2>
           <span className="font-body w-full px-10 flex justify-between"><p className="text-accent/70">Curated scents for every mood</p><button className="hover:underline cursor-pointer text-accent" onClick={() => setIsInViewAllMode(!isInViewAllMode)}>{isInViewAllMode ? 'View less' : 'View all'}</button></span>
           <div className="w-full h-full px-5 mt-10 flex flex-col gap-5 lg:grid lg:grid-cols-2 lg:gap-6">
             {
