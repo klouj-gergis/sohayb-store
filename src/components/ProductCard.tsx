1fa1ce88e88f22b88424bbb374021035f6984e40
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2026-05-31 09:23:13 +0300

    modified a productCard style

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,10 +12,12 @@ export default function ProductCard({ productData, currency }: { productData: {
       <div className="w-1/3 h-full bg-accent/30 rounded-lg overflow-hidden p-5">
         <img src={productData.image} alt={productData.name} className="h-full  object-cover rounded-lg" />
       </div>
-      <div className="w-2/3 h-full flex flex-col justify-around py-5">
-        <h3 className="text-md text-dark-accent font-semibold">{productData.name }</h3>
-      <p className="text-md text-accent lg:-mt-10">{productData.description}</p>
-      <span className="flex justify-between items-center mt-4">
+      <div className="w-2/3 h-full flex flex-col justify-between  pt-3">
+        <span className="flex flex-col gap-1">
+          <h3 className="text-md text-dark-accent font-semibold">{productData.name }</h3>
+      <p className="text-md text-accent">{productData.description}</p>
+        </span>
+      <span className="flex justify-between items-center">
         <p className="text-2xl font-bold text-dark-accent">{currency} {productData.price}</p>
         <button title="add to cart" type="button" className="p-2 bg-accent/30 text-dark-accent rounded-full hover:bg-accent/50 cursor-pointer transition-colors"><Plus /></button>
       </span>
